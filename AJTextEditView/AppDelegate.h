e991b1c0c45a850e652aca7bebdbe3a8d9b01aa3
hushijie <17744582425@163.com>
2017-07-03 11:47:07 +0800

    Initial Commit

---
+++ AJTextEditView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AJTextEditView
+//
+//  Created by JasonHu on 2017/7/3.
+//  Copyright © 2017年 AJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AJTextEditView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AJTextEditView
+//
+//  Created by JasonHu on 2017/7/3.
+//  Copyright © 2017年 AJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
